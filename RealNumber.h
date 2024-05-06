821cfc507d731855a385c3a38538076c3e649c71
Hoang C. Ta <taconghoang1103@gmail.com>
2024-05-06 17:19:18 +0700

    Init class Real Number

---
+++ RealNumber.h
@@ -0,0 +1,14 @@
+#ifndef PAILLIER_CRYPTOSYSTEM_REALNUMBER_H
+#define PAILLIER_CRYPTOSYSTEM_REALNUMBER_H
+
+#include "BigInteger.h"
+
+class RealNumber {
+private:
+    BigInteger n;
+    int exponent;
+public: 
+    
+};
+
+#endif //PAILLIER_CRYPTOSYSTEM_REALNUMBER_H
